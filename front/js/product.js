9ac6206215c31c75f124dc4704ba80e0be166fed
IM-98 <mansourilies@gmail.com>
2022-09-12 18:45:47 +0200

    implement addToCart logic

--- front/js/product.js
+++ front/js/product.js
@@ -7,11 +7,12 @@ const productPrice = document.getElementById("price")
 const productDescription = document.getElementById("description")
 const selectColors = document.querySelector("select")
 
+let shoppingCart = JSON.parse(localStorage.getItem("CART")) || [] 
+const addProduct = document.getElementById("addToCart")
 
 fetch(productInfo)
 .then(res => res.json())
 .then(product => {
-
     let imgProduct = document.createElement("img")
     imgProduct.src = product.imageUrl
     imgContainer.appendChild(imgProduct)
@@ -29,6 +30,37 @@ fetch(productInfo)
         productColorOption.value =  product.colors[i]
         productColorOption.innerText = product.colors[i]
         selectColors.appendChild(productColorOption)
+    }
+    
+    addProduct.addEventListener("click",  e => addToCart())
+
+    function addToCart() {
+
+        const productQuantitySelected = document.getElementById("quantity").value
+        const productColorsSelected = document.getElementById("colors").value
+
+        if( productQuantitySelected >=1 && productQuantitySelected < 100 && productColorsSelected !== undefined &&  productColorsSelected !== "" ){
+    
+            let productChoosen = {
+                color :productColorsSelected,
+                name : product.name,
+                price : product.price,
+                quantity : productQuantitySelected ,
+                imageSrc : product.imageUrl,
+                description : product.description,
+                id : productId
+            }
+            
+            shoppingCart.push(productChoosen)
+            localStorage.setItem("CART", JSON.stringify(shoppingCart))
+        
+           
+            console.log(shoppingCart)
+        }
+        else {
+            alert("vous devez selectionner une couleur et une quantité")
+        
+        }
     }
 
 })
